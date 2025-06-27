d3479e354aeac7ab6ad7ee30f458952f3239b9ee
Shelly <mhdyuong@gmail.com>
2025-06-27 15:44:33 -0700

    ui changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 DATA_FILE = 'recipes.json'
+RECIPES_PER_PAGE = 6
 
 def load_recipes():
     if os.path.exists(DATA_FILE):
@@ -19,13 +20,18 @@ def save_recipes():
 recipes = load_recipes()
 
 @app.route('/')
-def index():
-    return render_template('index.html', recipes=recipes)
+@app.route('/page/<int:page>')
+def index(page=1):
+    start = (page - 1) * RECIPES_PER_PAGE
+    end = start + RECIPES_PER_PAGE
+    total_pages = (len(recipes) + RECIPES_PER_PAGE - 1) // RECIPES_PER_PAGE
+    paginated = recipes[start:end]
+    return render_template('index.html', recipes=paginated, page=page, total_pages=total_pages)
 
 @app.route('/recipe/<int:recipe_id>')
 def recipe(recipe_id):
     if 0 <= recipe_id < len(recipes):
-        return render_template('recipe.html', recipe=recipes[recipe_id])
+        return render_template('recipe.html', recipe=recipes[recipe_id], recipe_id=recipe_id)
     return "Recipe not found", 404
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -43,5 +49,13 @@ def add_recipe():
         return redirect(url_for('index'))
     return render_template('add_recipe.html')
 
+@app.route('/delete/<int:recipe_id>', methods=['POST'])
+def delete_recipe(recipe_id):
+    if 0 <= recipe_id < len(recipes):
+        del recipes[recipe_id]
+        save_recipes()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
+
